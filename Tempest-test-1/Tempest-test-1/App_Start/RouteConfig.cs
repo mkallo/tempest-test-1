0b29b6bd8e3394cdde3b4ac9e2b103a9bab34e64
xkral <michalkral2011@gmail.com>
2016-05-17 21:07:02 +0200

    Final changes
    Zmena routeConfing na startPage, build poslednych zmien

--- Tempest-test-1/Tempest-test-1/App_Start/RouteConfig.cs
+++ Tempest-test-1/Tempest-test-1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Tempest_test_1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "EMPs", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
